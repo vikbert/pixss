ae1409b991bcf12d492fd2928973ffd032d37980
Xun Zhou <segentor@gmail.com>
2020-06-20 20:56:55 +0200

    updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 ;(function () {
   $('nav').on('click', '.sidebar-item', function (event) {
     const fancyClass = 'active'
-    console.log($('nav .sidebar-item'))
+
     $('nav .sidebar-item').removeClass(fancyClass)
     if (!$(this).hasClass('active')) {
       $(this).addClass('active')
