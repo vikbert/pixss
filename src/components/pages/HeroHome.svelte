2f6d7e17b4acca826d61d0550e2de8639435eee2
Xun Zhou <segentor@gmail.com>
2020-11-14 17:27:08 +0100

    updates

--- src/components/pages/HeroHome.svelte
+++ src/components/pages/HeroHome.svelte
@@ -2,7 +2,7 @@
   .root {
     display: grid;
     grid-gap: 1rem;
-    grid-template-columns: repeat(auto-fit, minmax(45rem, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(40rem, 1fr));
     margin: 4rem 0;
   }
   .box {
@@ -19,15 +19,17 @@
 </style>
 
 <div class="hero is-primary">
-  <div class="root">
-    <div class="box">
-      <img class="logo" src="./static/app.png" alt="" />
-      <h3 class="bold opacity-75">A pragmatic CSS for Minimalist</h3>
-      <h4 class="bold opacity-50">Small but good enough</h4>
-      <button class="is-rounded is-outlined">Download only ~5kb</button>
-    </div>
-    <div class="box">
-      <img class="hide-mobile" width="370" src="./static/rocket.svg" alt="" />
+  <div class="container">
+    <div class="root">
+      <div class="box">
+        <img class="logo" src="./static/app.png" alt="" />
+        <h3 class="bold opacity-75">A pragmatic CSS for Minimalist</h3>
+        <h4 class="bold opacity-50">Small but good enough</h4>
+        <button class="is-rounded is-outlined">Download only ~5kb</button>
+      </div>
+      <div class="box">
+        <img class="hide-mobile" width="370" src="./static/rocket.svg" alt="" />
+      </div>
     </div>
   </div>
 </div>
